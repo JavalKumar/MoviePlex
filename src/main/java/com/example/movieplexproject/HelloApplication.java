90dabb215f6fd16e45f947d6e6bde30e45cd9f85
“Javalkumar <W0845066@myscc.ca>
2024-10-27 16:09:18 -0400

    Made a simple structure for landing page. added title, added buttons.

--- src/main/java/com/example/movieplexproject/HelloApplication.java
+++ src/main/java/com/example/movieplexproject/HelloApplication.java
@@ -2,8 +2,12 @@ package com.example.movieplexproject;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,8 +16,24 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         BorderPane pane = new BorderPane();
-        Scene scene = new Scene(pane, 320, 240);
-        stage.setTitle("Hello!");
+
+        VBox titleBox = new VBox();
+        Text title = new Text("MOVIEPLEX");
+        titleBox.getChildren().add(title);
+        titleBox.setAlignment(Pos.TOP_CENTER);
+
+        VBox buttonBox = new VBox();
+        Button movies = new Button("Movies");
+        Button primeUsers = new Button("Prime Users");
+        Button employee = new Button("Employee");
+        buttonBox.getChildren().addAll(movies,primeUsers,employee);
+        buttonBox.setAlignment(Pos.CENTER);
+
+        pane.setTop(titleBox);
+        pane.setCenter(buttonBox);
+
+        Scene scene = new Scene(pane, 1024, 768);
+        stage.setTitle("MoviePlex");
         stage.setScene(scene);
         stage.show();
     }
