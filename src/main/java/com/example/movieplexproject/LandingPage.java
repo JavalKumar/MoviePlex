87d6472f0314b250ef566e7e6c4e778f82b19e0d
Georgina  Ayebiahwe <w0859914@myscc.ca>
2024-10-27 16:36:42 -0400

    removed the styles because this is a basic stage that will have only Movie tab, prime user tab and employee tab with a title called MOVIEPLEX

--- src/main/java/com/example/movieplexproject/LandingPage.java
+++ src/main/java/com/example/movieplexproject/LandingPage.java
@@ -1,62 +1,44 @@
 package com.example.movieplexproject;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class LandingPage extends Application {
-
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        //This is the title of the welcome page.
-        Text title = new Text("Welcome to MoviePlex!!");
-        title.setStyle("-fx-font-size: 40px; -fx-fill: gold; -fx-text-alignment: center;");
+    public void start(Stage stage) throws IOException {
+        BorderPane pane = new BorderPane();
 
+        VBox titleBox = new VBox();
+        Text title = new Text("MOVIEPLEX");
+        titleBox.getChildren().add(title);
+        titleBox.setAlignment(Pos.TOP_CENTER);
 
-        // these are the buttons for the first welcome page.
-        Button movieButton = new Button("Movie");
-        Button primeUserButton = new Button("Prime User");
-        Button employeeButton = new Button("Employee");
+        VBox buttonBox = new VBox();
+        Button movies = new Button("Movies");
+        Button primeUsers = new Button("Prime Users");
+        Button employee = new Button("Employee");
+        buttonBox.getChildren().addAll(movies,primeUsers,employee);
+        buttonBox.setAlignment(Pos.CENTER);
 
+        pane.setTop(titleBox);
+        pane.setCenter(buttonBox);
 
-        //An exit button to exit from the page
-        Button exitButton = new Button("Exit");
-
-        //Set on Action for the exit tab defines the action for the "Exit" button
-        exitButton.setOnAction(e -> {
-            Stage LandingPage = (Stage) exitButton.getScene().getWindow();
-            primaryStage.close();
-        });
-
-
-        // This is the VBox to hold the buttons
-        // 10 pixels spacing
-        VBox vbox = new VBox(10);
-        vbox.getChildren().addAll(movieButton, primeUserButton, employeeButton);
-        vbox.getChildren().add(exitButton);
-        exitButton.setStyle("-fx-alignment: top-left; -fx-padding: 10;");
-
-        // Center alignment
-        vbox.setStyle("-fx-alignment: center;");
-
-        // LandingPageLayout // 20 pixels spacing
-        VBox LandingPageLayout = new VBox(20);
-        LandingPageLayout.getChildren().addAll(title, vbox, exitButton);
-        LandingPageLayout.setStyle("-fx-alignment: center; -fx-padding: 20;");
-
-        // Scene
-        Scene scene = new Scene(LandingPageLayout, 1600, 900);
-        primaryStage.setTitle("MoviePlex!!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-
-
+        Scene scene = new Scene(pane, 1024, 768);
+        stage.setTitle("MoviePlex");
+        stage.setScene(scene);
+        stage.show();
     }
+
     public static void main(String[] args) {
-        launch(args);
-    }
-
+        launch();
+}
 }
