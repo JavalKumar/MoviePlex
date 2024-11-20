2af45c8bccc9008403e70bc214b00d52c006a93b
Georgina  Ayebiahwe <w0859914@myscc.ca>
2024-11-20 12:17:56 -0500

    checked to see if the connection is null, created connection using the Drivermanager passing the connection URL, database, username, and password

--- src/main/java/com/example/movieplexproject/Database/Database.java
+++ src/main/java/com/example/movieplexproject/Database/Database.java
@@ -1,12 +1,28 @@
 package com.example.movieplexproject.Database;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+
+import static java.lang.Class.forName;
 
 public class Database {
     private static Database instance;
     private Connection connection;
 
     private Database(){
+if (connection == null){
+    try {
+        Class.forName("com.mysql.jdbc.Driver");
+        connection = DriverManager.getConnection("jdbc:mysql://php.scweb.ca" + Const.DB_NAME + "?useSSL=false",
+                Const.DB_USER, Const.DB_PASS);
+        System.out.println("Connection Successfully Connected");
+    }
+    catch (Exception e) {
+        e.printStackTrace();
+    }
 
     }
 }
+
+    }
+
